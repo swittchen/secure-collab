67a4cb2c73a95189921963b36c76aad18471f9bb
Wittchen <swittche@ipb-halle.de>
2025-05-21 09:04:39 +0200

    Add CustomOAuth2UserService for Google/GitHub login, Loads user info from OAuth2 provider, Saves new users in database with VIEWER role, Returns authenticated DefaultOAuth2User with ROLE_VIEWER

--- src/main/java/com/securecollab/user/CustomOAuth2UserService.java
+++ src/main/java/com/securecollab/user/CustomOAuth2UserService.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +21,9 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
 
         String email = oAuth2User.getAttribute("email");
+        if (email == null) {
+            throw new RuntimeException("OAuth2 provider did not return email");
+        }
 
         userRepository.findByEmail(email).orElseGet(() -> {
             User newUser = new User();
@@ -27,6 +33,10 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
             return userRepository.save(newUser);
         });
 
-        return oAuth2User;
+        return new DefaultOAuth2User(
+                Collections.singleton(() -> "ROLE_VIEWER"),
+                oAuth2User.getAttributes(),
+                "email"
+        );
     }
 }
